d9b146dbcd85c5318c6f0d82f087528dfe603bbb
Pandora_300 <adri.mathieu@hotmail.fr>
2024-02-01 22:49:32 +0100

    Strategy changed, only calcul visible cells in the 'Face' object

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
+import copy
+
+
 class Cell:
-    def __init__(self, value: int, orientation: int):
+    def __init__(self, value: int, valueOrientation: int):
         # 'Cell' object attributes affectations
         self.__value: int = value
-        self.__orientation: int = orientation
+        self.__valueOrientation: int = valueOrientation
 
     def __repr__(self):
         # 'Cell' object representation in string
-        return f"<Cell object with value={self.__value} and orientation={self.__orientation}>"
+        return f"<Cell object with value={self.__value} and valueOrientation={self.__valueOrientation}>"
 
     @property
     def value(self):
@@ -14,96 +17,93 @@ class Cell:
         return self.__value
 
     @property
-    def orientation(self):
-        # Return the internal 'orientation' variable
-        return self.__orientation
+    def valueOrientation(self):
+        # Return the internal 'valueOrientation' variable
+        return self.__valueOrientation
 
 
-class Piece:
+class Face:
+    PIECE_COUNT = 3
     PIECE_VALUE_INDEX = 0
     PIECE_ORIENTATION_INDEX = 1
-    PIECE_FACING_INDEX = 2
 
-    def __init__(self, pieceValues: list[tuple[int | str]]):
-        # 'Piece' object attribute initialization
-        self.__cells: dict[str, Cell] = dict()
+    def __init__(self, pieceValues: list[list[tuple[int, int]]]):
+        # 'Face' object attribute initialization
+        self.__cells: list[list[Cell]] = [[Cell(value=0, valueOrientation=0) for _ in range(self.PIECE_COUNT)]
+                                          for _ in range(self.PIECE_COUNT)]
 
-        # Going through the input to store information using attributes
-        for indexValue in range(len(pieceValues)):
-            self.__cells[pieceValues[indexValue][self.PIECE_FACING_INDEX]] = \
-                (Cell(value=pieceValues[indexValue][self.PIECE_VALUE_INDEX],
-                      orientation=pieceValues[indexValue][self.PIECE_ORIENTATION_INDEX]))
+        # Going through the input to store information in the '__cells' attribute
+        for xIndex in range(self.PIECE_COUNT):
+            for yIndex in range(self.PIECE_COUNT):
+                self.__cells[xIndex][yIndex] = \
+                    Cell(value=pieceValues[xIndex][yIndex][self.PIECE_VALUE_INDEX],
+                         valueOrientation=pieceValues[xIndex][yIndex][self.PIECE_ORIENTATION_INDEX])
 
     def __repr__(self):
-        # 'Piece' object representation in string
-        return f"<Piece object with {len(self.__cells)} cells>"
+        # 'Face' object representation
+        return f"<Piece object with {self.PIECE_COUNT}x{self.PIECE_COUNT} cells>"
 
-    def cell(self, index: int):
+    def print(self):
+        # 'Face' object displayed in string
+        print("'Face' object displayed in string :")
+        print('\n'.join(str([self.__cells[xIndex][yIndex].value for yIndex in range(self.PIECE_COUNT)])
+                        for xIndex in range(self.PIECE_COUNT)))
+
+    def cell(self, xIndex: int, yIndex: int):
         # Return one of the 'cell' variable
-        return self.__cells[index]
+        return self.__cells[xIndex][yIndex]
 
     @property
     def cells(self):
         # Return the internal 'cells' variable
         return self.__cells
 
+    def rotateClockwise(self):
+        # Saving the current state of the '__cells' attribute
+        cells = copy.deepcopy(self.__cells)
 
-class Face:
-    def __init__(self, faceValues: list[list[tuple[int, int]]]):
-        # 'Face' object attribute initialization
-        self.__pieces: list[Piece] = list()
+        # Going through the input to store information in the '__cells' attribute
+        for xIndex in range(self.PIECE_COUNT):
+            for yIndex in range(self.PIECE_COUNT):
+                self.__cells[yIndex][self.PIECE_COUNT - xIndex - 1] = cells[xIndex][yIndex]
 
-        # Going through the input to store information using attributes
-        for indexValue in range(len(faceValues)):
-            self.__pieces.append(Piece(pieceValues=faceValues[indexValue]))
+    def rotateCounterClockwise(self):
+        # Saving the current state of the '__cells' attribute
+        cells = copy.deepcopy(self.__cells)
 
-    def __repr__(self):
-        # 'Face' object representation in string
-        return f"<Piece object with {len(self.__pieces)} pieces>"
+        # Going through the input to store information in the '__cells' attribute
+        for xIndex in range(self.PIECE_COUNT):
+            for yIndex in range(self.PIECE_COUNT):
+                self.__cells[self.PIECE_COUNT - yIndex - 1][xIndex] = cells[xIndex][yIndex]
 
-    def piece(self, index: int):
-        # Return one of the 'piece' variable
-        return self.__pieces[index]
+    def rotateUTurn(self):
+        # Saving the current state of the '__cells' attribute
+        cells = copy.deepcopy(self.__cells)
 
-    @property
-    def pieces(self):
-        # Return the internal 'pieces' variable
-        return self.__pieces
-
-    @property
-    def face_pieces(self):
-        # Return the internal 'pieces' variable
-        return self.__pieces
+        # Going through the input to store information in the '__cells' attribute
+        for xIndex in range(self.PIECE_COUNT):
+            for yIndex in range(self.PIECE_COUNT):
+                self.__cells[self.PIECE_COUNT - xIndex - 1][self.PIECE_COUNT - yIndex - 1] = cells[xIndex][yIndex]
 
 
 if __name__ == "__main__":
-    FacesValues = [[(3, 3),
-                    (8, 0),
-                    (5, 1)],
-                   [(0, 0),
-                    (0, 2)],
-                   [(1, 1),
-                    (3, 0),
-                    (0, 0)],
-                   [(8, 2),
-                    (8, 0)],
-                   [(2, 3)],
-                   [(9, 1),
-                    (0, 3)],
-                   [(6, 1),
-                    (7, 0),
-                    (7, 0)],
-                   [(3, 1),
-                    (2, 0)],
-                   [(9, 0),
-                    (4, 0),
-                    (9, 1)]
-                   ]
+    piece_Values = [[(1, 2),
+                     (5, 1),
+                     (0, 1)],
+                    [(6, 1),
+                     (6, 2),
+                     (2, 1)],
+                    [(6, 3),
+                     (4, 0),
+                     (9, 0)]]
 
-    print(FacesValues)
-
-    piece1 = Face(faceValues=FacesValues)
-
+    print(piece_Values)
+    piece1 = Face(pieceValues=piece_Values)
     print(piece1)
-    print(piece1.pieces)
-    print(piece1.piece(index=2))
+    piece1.print()
+    piece1.rotateClockwise()
+    piece1.print()
+    piece1.rotateCounterClockwise()
+    piece1.print()
+    piece1.rotateUTurn()
+    piece1.print()
