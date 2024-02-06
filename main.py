864a16b73534bf80f404e34cb1e55ea9d460a925
Pandora_300 <adri.mathieu@hotmail.fr>
2024-02-06 01:25:08 +0100

    Attempting to create something

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ class Face:
     def __init__(self, pieceValues: list[list[tuple[int, int]]], facing: Facing):
         # 'Face' object attribute initialization
         self.__facing = facing
-        self.__cells: list[list[Cell | None]] = [[None] * self.PIECE_COUNT] * self.PIECE_COUNT
+        self.__cells: list[list[Cell | None]] = [[None] * self.PIECE_COUNT for _ in range(self.PIECE_COUNT)]
 
         # Going through the input to store information in the '__cells' attribute
         for xIndex in range(self.PIECE_COUNT):
@@ -58,7 +58,7 @@ class Face:
 
     def __repr__(self):
         # 'Face' object representation
-        return f"<Piece object with {self.PIECE_COUNT}x{self.PIECE_COUNT} cells facing {self.__facing}>"
+        return f"<Piece object with {self.PIECE_COUNT}x{self.PIECE_COUNT} cells facing {self.__facing.name}>"
 
     @property
     def cells(self):
@@ -147,49 +147,95 @@ class Face:
 class Cube:
     FACE_COUNT = 6
 
-    def __init__(self, faces_values: dict[Face]):
+    def __init__(self, faces_values: dict[Facing, list[list[tuple[int, Orientation]]]]):
         self.__faces = [Face(facing=facing, pieceValues=face_values)
                         for facing, face_values in faces_values.items()]
 
-    def rotate_clockwise(self):
+    @property
+    def faces(self):
+        # Return the internal 'faces' variable
+        return self.__faces
+
+    def rotate_clockwise(self, facing: Facing):
+        #
+        rotating_edges_values: list[list[Cell | None]] = list()
+        neighbours_edges_facing = [_facing for _facing in Facing if _facing not in (facing, (facing + 3))]
+
+        #
+        self.__faces[facing].rotate_clockwise()
+
+        #
+        for facing_index in neighbours_edges_facing:
+            rotating_edges_values.append(self.__faces[facing].get_edge_values(facing=facing_index))
+
+        #
+        for facing_index in range(len(neighbours_edges_facing)):
+            self.__faces[facing].set_edge_values(facing=neighbours_edges_facing[facing_index],
+                                                 edgesValues=rotating_edges_values[facing_index])
+
+    def rotate_counter_clockwise(self, facing: Facing):
         pass
 
-    def rotate_counter_clockwise(self):
-        pass
-
-    def rotate_u_turn(self):
+    def rotate_u_turn(self, facing: Facing):
         pass
 
 
 if __name__ == "__main__":
-    piece_values = [[(1, Orientation.DOWN),
-                     (5, Orientation.RIGHT),
-                     (0, Orientation.RIGHT)],
-                    [(6, Orientation.RIGHT),
-                     (6, Orientation.DOWN),
-                     (2, Orientation.RIGHT)],
-                    [(6, Orientation.LEFT),
-                     (4, Orientation.UP),
-                     (9, Orientation.UP)]]
+    _faces_values = {Facing.FRONT: [[(4, Orientation.UP),
+                                     (3, Orientation.UP),
+                                     (6, Orientation.UP)],
+                                    [(7, Orientation.UP),
+                                     (5, Orientation.UP),
+                                     (1, Orientation.UP)],
+                                    [(9, Orientation.UP),
+                                     (2, Orientation.UP),
+                                     (8, Orientation.UP)]],
+                     Facing.DOWN: [[(3, Orientation.UP),
+                                    (8, Orientation.UP),
+                                    (7, Orientation.UP)],
+                                   [(2, Orientation.UP),
+                                    (1, Orientation.UP),
+                                    (9, Orientation.UP)],
+                                   [(5, Orientation.UP),
+                                    (6, Orientation.UP),
+                                    (4, Orientation.UP)]],
+                     Facing.RIGHT: [[(9, Orientation.UP),
+                                     (7, Orientation.UP),
+                                     (1, Orientation.UP)],
+                                    [(2, Orientation.UP),
+                                     (4, Orientation.UP),
+                                     (8, Orientation.UP)],
+                                    [(5, Orientation.UP),
+                                     (3, Orientation.UP),
+                                     (6, Orientation.UP)]],
+                     Facing.LEFT: [[(1, Orientation.UP),
+                                    (4, Orientation.UP),
+                                    (2, Orientation.UP)],
+                                   [(8, Orientation.UP),
+                                    (7, Orientation.UP),
+                                    (3, Orientation.UP)],
+                                   [(6, Orientation.UP),
+                                    (9, Orientation.UP),
+                                    (5, Orientation.UP)]],
+                     Facing.UP: [[(9, Orientation.UP),
+                                  (8, Orientation.UP),
+                                  (7, Orientation.UP)],
+                                 [(3, Orientation.UP),
+                                  (6, Orientation.UP),
+                                  (4, Orientation.UP)],
+                                 [(2, Orientation.UP),
+                                  (5, Orientation.UP),
+                                  (1, Orientation.UP)]],
+                     Facing.BACK: [[(4, Orientation.DOWN),
+                                    (2, Orientation.DOWN),
+                                    (8, Orientation.DOWN)],
+                                   [(5, Orientation.DOWN),
+                                    (1, Orientation.DOWN),
+                                    (6, Orientation.DOWN)],
+                                   [(7, Orientation.DOWN),
+                                    (9, Orientation.DOWN),
+                                    (3, Orientation.DOWN)]]}
 
-    new_edge = [Cell(value=value, value_orientation=value_orientation)
-                for value, value_orientation in [(6, Orientation.DOWN),
-                                                 (6, Orientation.RIGHT),
-                                                 (6, Orientation.UP)]]
-
-    print(piece_values)
-    piece1 = Face(pieceValues=piece_values, facing=Facing.FRONT)
-    print(piece1)
-    piece1.print()
-    piece1.rotate_clockwise()
-    piece1.print()
-    print(piece1.get_edge_values(facing=Facing.UP))
-    piece1.rotate_counter_clockwise()
-    piece1.print()
-    print(piece1.get_edge_values(facing=Facing.RIGHT))
-    piece1.rotate_u_turn()
-    piece1.print()
-    print(piece1.get_edge_values(facing=Facing.DOWN))
-    piece1.print()
-    piece1.set_edge_values(Facing.LEFT, new_edge)
-    piece1.print()
+    cube_1 = Cube(faces_values=_faces_values)
+    cube_1.rotate_clockwise(facing=Facing.FRONT)
+    print(cube_1.faces)
